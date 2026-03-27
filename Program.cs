47aa77f0252c07c439ec58d5ead6659967124ddb
miggyreyesss <miggyreyes0618@gmail.com>
2026-03-27 21:41:39 +0800

    Revert added welcome message

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Welcome to Simple Calculator!");
-
         Console.Write("Enter first number: ");
         double num1 = Convert.ToDouble(Console.ReadLine());
 
